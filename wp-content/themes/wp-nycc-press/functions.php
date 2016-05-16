3b4681f851dbf877d8264744b9cfb63e6098d5ea
Andy Cochran <acochran@council.nyc.gov>
2016-05-16 16:53:47 -0400

    add leg front page template

--- wp-content/themes/wp-nycc-press/functions.php
+++ wp-content/themes/wp-nycc-press/functions.php
@@ -6,3 +6,20 @@ require_once(get_template_directory().'/assets/functions/remove-in-child-theme.p
 
 // Press-specific functions
 // require_once(get_stylesheet_directory().'/functions/options.php');        // Options
+
+// Hide admin stuff
+function remove_land_use_menus(){
+  remove_menu_page( 'plugins.php' );
+  remove_menu_page( 'tools.php' );
+  remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+}
+add_action( 'admin_menu', 'remove_land_use_menus' );
+
+// Remove admin bar links
+function remove_land_use_admin_bar_links() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('updates');
+    $wp_admin_bar->remove_menu('comments');
+    $wp_admin_bar->remove_menu('new-user');
+}
+add_action( 'wp_before_admin_bar_render', 'remove_land_use_admin_bar_links' );
