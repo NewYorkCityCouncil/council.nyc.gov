21b6a6e3654287961fba8e57bbf23777d069ce3d
Andy Cochran <acochran@council.nyc.gov>
2016-05-16 15:38:08 -0400

    add Press theme

---
+++ wp-content/themes/wp-nycc-press/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// Remove parent theme's admin pages, nav menus, and page tempaltes
+require_once(get_template_directory().'/assets/functions/remove-in-child-theme.php');
+
+
+// Press-specific functions
+// require_once(get_stylesheet_directory().'/functions/options.php');        // Options
