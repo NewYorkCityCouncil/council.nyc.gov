f3bedc243aa89a026924952a5ab729d6c2c2455d
Andy Cochran <acochran@council.nyc.gov>
2016-06-07 10:52:46 -0400

    add Google Analytics script to footer

--- wp-content/themes/wp-nycc/footer.php
+++ wp-content/themes/wp-nycc/footer.php
@@ -43,5 +43,15 @@
     <?php if ( is_post_type_archive('nycc_pb_ballot_item') ) { get_template_part( 'pb-map-scripts' ); } ?>
     <?php if ( is_page_template( 'page-pbdistricts.php' ) ) { get_template_part( 'geolocate-scripts' ); } ?>
 
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-68577323-2', 'auto');
+      ga('send', 'pageview');
+    </script>
+
   </body>
 </html>
