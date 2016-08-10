8b5a865eaed64c754119126a7785b9ee3812c330
Andy Cochran <acochran@council.nyc.gov>
2016-08-10 13:37:33 -0400

    add district archive template

---
+++ wp-content/themes/wp-nycc-district/archive.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+      <header>
+        <h1 class="header-small"><?php the_archive_title();?></h1>
+        <?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
+        <hr>
+      </header>
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'loop', 'archive' ); ?>
+
+      <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
+
+      <?php nycc_page_navi(); ?>
+
+    </div>
+
+    <?php get_sidebar(); ?>
+
+  </div>
+
+<?php get_footer(); ?>
