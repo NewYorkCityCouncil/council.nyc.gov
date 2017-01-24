2152aadc2b5a97690bb17ddfe7c0b82f1cbe4a36
Andy Cochran <acochran@council.nyc.gov>
2017-01-24 13:29:29 -0500

    fix cache for enqueue styles w filemtime() and network_site_url()

--- wp-content/themes/wp-nycc/assets/functions/enqueue-scripts.php
+++ wp-content/themes/wp-nycc/assets/functions/enqueue-scripts.php
@@ -2,14 +2,16 @@
 function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
 
+  $assets_uri = network_site_url() . 'wp-content/themes/wp-nycc/assets/';
+
   // Add Foundation scripts file in the footer
-  wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.min.js', array( 'jquery' ), '6.2', true );
+  wp_enqueue_script( 'foundation-js', $assets_uri . 'js/foundation.min.js', array( 'jquery' ), '6.2', true );
 
   // Add JavaScript in the footer
-  wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), '', true );
+  wp_enqueue_script( 'site-js', $assets_uri . 'js/scripts.min.js', array(), '', true );
 
   // Register main stylesheet
-  wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/app.min.css', array('dashicons'), '', 'all' );
+  wp_enqueue_style( 'site-css', $assets_uri . 'css/app.min.css', array('dashicons'), filemtime( get_template_directory() . '/assets/css/app.min.css' ), 'all' );
 
 }
 add_action('wp_enqueue_scripts', 'site_scripts', 999);
