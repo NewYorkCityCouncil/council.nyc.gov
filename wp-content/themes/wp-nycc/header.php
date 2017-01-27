8d29e3a5e80768ce619494588eede0f50379e6ba
Andy Cochran <acochran@council.nyc.gov>
2017-01-27 10:59:58 -0500

    use http in header home_url

--- wp-content/themes/wp-nycc/header.php
+++ wp-content/themes/wp-nycc/header.php
@@ -43,7 +43,7 @@
               <div class="columns">
                 <?php switch_to_blog(1); ?>
                 <div class="top-bar-title">
-                  <strong class="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/nyc-seal-blue.png"><?php bloginfo('name'); ?></a></strong>
+                  <strong class="site-logo"><a href="<?php echo esc_url( home_url( '/', 'http' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/nyc-seal-blue.png"><?php bloginfo('name'); ?></a></strong>
                 </div>
                 <span class="responsive-menu-toggle" data-responsive-toggle="responsive-menu" data-hide-for="large"><span class="menu-icon dark" data-toggle></span></span>
                 <div id="responsive-menu">
