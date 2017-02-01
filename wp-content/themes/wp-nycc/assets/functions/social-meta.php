102a33d164f722b56cefba9af100a6c082c1e56b
Andy Cochran <acochran@council.nyc.gov>
2017-02-01 13:12:03 -0500

    set social description if no excerpt

--- wp-content/themes/wp-nycc/assets/functions/social-meta.php
+++ wp-content/themes/wp-nycc/assets/functions/social-meta.php
@@ -1,7 +1,10 @@
 <?php
 
 function insert_share_meta_in_head() {
+
   global $post;
+
+  // Define the image
   if ( !has_post_thumbnail() ) {
     $share_image = get_template_directory_uri() . '/assets/images/social-img-1024x512.jpg';
   }
@@ -9,6 +12,14 @@ function insert_share_meta_in_head() {
     $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
     $share_image = esc_attr( $thumbnail_src[0] );
   }
+
+  // Define the description
+  if ( empty( $post->post_excerpt ) ) {
+    $social_description = wp_kses_post( wp_trim_words( $post->post_content,'55','' ) );
+  } else {
+    $social_description = wp_kses_post( $post->post_excerpt );
+  }
+
   ?>
 
   <!-- Social Share Images -->
@@ -16,15 +27,15 @@ function insert_share_meta_in_head() {
   <meta property="og:type"         content="website" />
   <meta property="og:title"        content="<?php the_title(); ?>" />
   <meta property="og:site_name"    content="<?php bloginfo('name'); ?>"/>
-  <meta property="og:description"  content="<?php echo get_the_excerpt(); ?>" />
-  <meta property="og:image"        content="<?php echo $share_image ?>"/>
+  <meta property="og:description"  content="<?php echo $social_description; ?>" />
+  <meta property="og:image"        content="<?php echo $share_image; ?>"/>
 
   <meta name="twitter:card"        content="summary_large_image" />
   <meta name="twitter:site"        content="@NYCCouncil" />
   <meta name="twitter:creator"     content="@NYCCouncil" />
   <meta name="twitter:title"       content="<?php the_title(); ?>" />
-  <meta name="twitter:description" content="<?php echo get_the_excerpt(); ?>" />
-  <meta name="twitter:image"       content="<?php echo $share_image ?>" />
+  <meta name="twitter:description" content="<?php echo $social_description; ?>" />
+  <meta name="twitter:image"       content="<?php echo $share_image; ?>" />
 
   <?php
 }
