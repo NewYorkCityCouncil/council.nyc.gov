aa024d915f0bb681d21948a1bc8616f9ab6bbf0a
Andy Cochran <acochran@council.nyc.gov>
2017-02-22 17:16:39 -0500

    clean up Email Activity Log mu plugin

--- wp-content/mu-plugins/nycc-email-activity-log.php
+++ wp-content/mu-plugins/nycc-email-activity-log.php
@@ -9,33 +9,6 @@ License: GNU General Public License & MIT
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-// function nycc_email_activity_log( $post_id, $post, $update ) {
-//
-//     // don't send email if post is being autosaved or isn't published
-//     if ( ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) || $post->post_status === 'auto-draft' )
-//         return;
-//
-//     // don't send email if post isn't published
-//     if ( $new_status !== 'publish' || $post->post_status === 'publish' )
-//         return;
-//
-//     // don't send email if user is Superadmin
-//     // if ( is_super_admin() ) return;
-//
-//     $emailto = get_option( 'admin_email' );
-//     $subject = 'Content Updated';
-//     $message = 'View it: ' . get_permalink( $post->ID ) . "\nEdit it: " . get_edit_post_link( $post->ID );
-//
-//     wp_mail( $emailto, $subject, $message );
-//
-// }
-// add_action( 'save_post', 'nycc_email_activity_log', 10, 3 );
-
-
-
-
-
-
 function nycc_email_activity_log( $post_id, $post_after ) {
 
     if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return; // don't send email if post is being autosaved or isn't published
