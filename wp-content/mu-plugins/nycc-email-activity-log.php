2db143852ae5c5c8319c35cc82c988b75b80771f
Andy Cochran <acochran@council.nyc.gov>
2017-03-06 14:41:36 -0500

    update Email Activity Log plugin

--- wp-content/mu-plugins/nycc-email-activity-log.php
+++ wp-content/mu-plugins/nycc-email-activity-log.php
@@ -9,23 +9,31 @@ License: GNU General Public License & MIT
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-function nycc_email_activity_log( $post_id, $post_after ) {
+function nycc_email_activity_log( $post_id, $post_after, $post_before ) {
 
-    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return; // don't send email if post is being autosaved or isn't published
-    if ( get_post_status($post_id) !== 'publish' ) return; // don't send email if post isn't published
-    if ( is_super_admin() ) return; // don't send email if user is Superadmin
+    // don't send email if...
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return; // post is being autosaved
+    if ( get_post_status($post_id) !== 'publish' ) return; // post isn't published
+    if ( is_super_admin() ) return; // user is Superadmin
     $theme = wp_get_theme();
-    if ( 'NYCC Press' == $theme->name ) return; // don't send email if it's the Press theme
+    if ( 'NYCC Press' == $theme->name ) return; // it's the Press theme
 
-    $post = $post_after;
-
+    // Set the to address
     $emailto  = get_option( 'admin_email' );
-    $subject = "Content Updated";
-    $message = $post->post_title . "\nView it: " . get_permalink( $post->ID ) . "\nEdit it: " . get_edit_post_link( $post->ID );
+
+    // Set the subject line
+    if ( $post_before->post_status === 'publish' && $post_before->post_status === 'publish' ) {
+      $subject = "Content Updated";
+    } else {
+      $subject = "New Content";
+    }
+
+    // Set the message
+    $message = "Title:\n" . $post_after->post_title . "\n\nView it:\n" . get_permalink( $post_after->ID ) . "\n\nEdit it:\n" . get_edit_post_link( $post_after->ID );
 
     if ( did_action('post_updated') == 1 ) {
         wp_mail( $emailto, $subject, $message );
     }
 
 }
-add_action('post_updated', 'nycc_email_activity_log', 10, 2);
+add_action('post_updated', 'nycc_email_activity_log', 10, 3);
