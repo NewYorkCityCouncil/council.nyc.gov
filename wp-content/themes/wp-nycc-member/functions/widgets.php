78ea5fa7100dde461f25046eb4c7604247efebc7
Andy Cochran <acochran@council.nyc.gov>
2017-04-04 12:41:33 -0400

    cleanup sidebar registration

--- wp-content/themes/wp-nycc-member/functions/widgets.php
+++ wp-content/themes/wp-nycc-member/functions/widgets.php
@@ -1,21 +1,5 @@
 <?php
 
-// Register sidebars
-function nycc_register_sidebars() {
-    register_sidebar(
-        array(
-            'id' => 'posts-sidebar',
-            'name' => __( 'Sidebar', 'nycc' ),
-            'description' => __( 'Widget Area', 'nycc' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h4 class="widget-title">',
-            'after_title' => '</h4>'
-        )
-    );
-}
-add_action( 'widgets_init', 'nycc_register_sidebars' );
-
 // Unregister widgets
 function nycc_unregister_press_widgets() {
   unregister_widget('WP_Nav_Menu_Widget');
