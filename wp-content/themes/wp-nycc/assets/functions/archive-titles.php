22be8a4997af1597f9dee93d59e24165b61d2716
Andy Cochran <acochran@council.nyc.gov>
2017-04-04 16:44:31 -0400

    fix cat title

--- wp-content/themes/wp-nycc/assets/functions/archive-titles.php
+++ wp-content/themes/wp-nycc/assets/functions/archive-titles.php
@@ -4,7 +4,7 @@
 add_filter( 'get_the_archive_title', function ($title) {
   if ( is_category() ) {
     $cat_title = single_cat_title( '', false );
-    $title = '<small>Tag: </small><em>' . $cat_title . '</em>';
+    $title = '<small>Category: </small><em>' . $cat_title . '</em>';
   } elseif ( is_tag() ) {
     $tag_title = single_tag_title( '', false );
     $title = '<small>Tag: </small><em>' . $tag_title . '</em>';
