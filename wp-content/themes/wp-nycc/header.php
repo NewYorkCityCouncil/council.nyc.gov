2bf9319403bf703b5d95057bba4397e8714b2cc6
Jonathan Chei <JChei@IT-Admins-MacBook-Pro.local>
2018-02-05 11:57:52 -0500

    adding google tag manager

--- wp-content/themes/wp-nycc/header.php
+++ wp-content/themes/wp-nycc/header.php
@@ -15,6 +15,13 @@
 
     <!-- Google Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TCN4XTT');</script>
+    <!-- End Google Tag Manager -->
 
     <!-- Carto -->
     <link rel="stylesheet" href="https://cartodb-libs.global.ssl.fastly.net/cartodb.js/v3/3.15/themes/css/cartodb.css" />
@@ -22,11 +29,14 @@
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
 
     <?php wp_head(); ?>
-    <title>New York City Council</title>
+    <script>jQuery(document).ready(function(){jQuery('#archive .dismiss span').on('click',function(){jQuery('#archive').animate({'opacity': 0}, 300)})});</script>
   </head>
 
   <body <?php body_class(); ?>>
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCN4XTT"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php if ( wp_get_theme()->get('Name') == 'NYCC Member' ) {} else { ?>
     <div id="map-container" class="short">
       <div id="map"></div>
