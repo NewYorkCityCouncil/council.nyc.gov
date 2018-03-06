80d11b6c84ad0a30bcd3db3a76a4e56ac5aaff6b
JC-NYCC-1227 <jchei@council.nyc.gov>
2018-03-06 15:44:28 -0500

    more afternoon bugs

--- wp-content/themes/wp-nycc/front-page.php
+++ wp-content/themes/wp-nycc/front-page.php
@@ -140,7 +140,7 @@
                 meetingHour = parseInt(hearing.EventTime.split(" ")[0].split(":")[0]);
                 meetingMinute = parseInt(hearing.EventTime.split(" ")[0].split(":")[1]);
                 hearing.EventAgendaFile !== null ? agendaLink = hearing.EventAgendaFile : agendaLink = "#";
-                midDay === "PM" ? meetingHour += 12 : meetingHour;
+                midDay === "PM" && meetingHour !== 12 ? meetingHour += 12 : meetingHour;
                 if((nowHour < meetingHour) || (nowHour === meetingHour && nowMinute <= meetingMinute)){ // if scheduled meeting has not happened yet
                   if(hearing.EventAgendaStatusName.toLowerCase() === "deferred"){
                     jQuery("#front-page-hearings").append("<div class='columns column-block' style='margin-bottom:10px;'><a href='"+agendaLink+"' target='_blank'><strong>"+hearing.EventBodyName+"</strong></a><br><i class='fa fa-clock-o' aria-hidden='true'></i> <small><s>"+hearing.EventTime+"</s> Deferred</small><br><i class='fa fa-map-marker' aria-hidden='true'></i> <small>"+hearing.EventLocation+"</small></div>");
