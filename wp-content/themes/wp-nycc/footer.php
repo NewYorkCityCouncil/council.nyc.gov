19df1b8642040dfa9af0aa760775092dcb6fe75e
JC-NYCC-1227 <jchei@council.nyc.gov>
2019-04-09 12:45:24 -0400

    script to capture outbound clicks on budget page

--- wp-content/themes/wp-nycc/footer.php
+++ wp-content/themes/wp-nycc/footer.php
@@ -32,6 +32,31 @@
       ga('create', 'UA-68577323-2', 'auto');
       ga('send', 'pageview');
     </script>
-
+    <script>
+      /**
+      * Function that captures a click on an outbound link in Analytics.
+      * This function takes a valid URL string as an argument, and uses that URL string
+      * as the event label. Setting the transport method to 'beacon' lets the hit be sent
+      * using 'navigator.sendBeacon' in browser that support it.
+      */
+      var captureOutboundLink = function(url) {
+        if(url == "http://www.nyc.gov/html/citycouncil/html/budget/expense_funding.shtml"){
+          ga('send', 'event', 'Outbound - Expense Funding', 'click', url, {
+            'transport': 'beacon',
+            'hitCallback': function(){document.location = url;}
+          });
+        } else if (url == "http://www.nyc.gov/html/citycouncil/html/budget/capital_funding.shtml"){
+          ga('send', 'event', 'Outbound - Capital Funding', 'click', url, {
+            'transport': 'beacon',
+            'hitCallback': function(){document.location = url;}
+          });
+        } else if (url == "https://data.cityofnewyork.us/City-Government/New-York-City-Council-Discretionary-Funding/4d7f-74pe"){
+          ga('send', 'event', 'Outbound - Discretionary Funding', 'click', url, {
+            'transport': 'beacon',
+            'hitCallback': function(){document.location = url;}
+          });
+        }
+      }
+    </script>
   </body>
 </html>
