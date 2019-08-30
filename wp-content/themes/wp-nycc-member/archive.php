85b24aa120a2786dc528afe5abd584274bf8486a
JC-NYCC-1227 <jchei@council.nyc.gov>
2019-08-30 16:30:25 -0400

    removal of header

--- wp-content/themes/wp-nycc-member/archive.php
+++ wp-content/themes/wp-nycc-member/archive.php
@@ -3,11 +3,11 @@
   <div class="row">
     <div class="columns medium-8 large-9 xxlarge-8">
 
-      <header>
-        <h1 class="header-small"><?php the_archive_title();?></h1>
-        <?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
+      <!-- <header>
+        <h1 class="header-small"><#?php the_archive_title();?></h1>
+        <#?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
         <hr>
-      </header>
+      </header> -->
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
