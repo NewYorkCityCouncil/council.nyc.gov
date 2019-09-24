833410c7c1e1f92504c1886f2e5ec906911d69a6
JC-NYCC-1227 <jchei@council.nyc.gov>
2019-09-24 17:23:17 -0400

    label for search bar

--- wp-content/themes/wp-nycc/page-listdistricts.php
+++ wp-content/themes/wp-nycc/page-listdistricts.php
@@ -24,7 +24,7 @@
             <div class="columns large-8 xxlarge-12 scrollable">
               <input type="text" aria-hidden="true" style="right:1000%;position:absolute;" value="" id="clipboard-copy">
               <form role="search" id="list-search" style="position:relative;">
-                <input type="text" id="list-search-input" class="-no-margin -search -search--no-submit" placeholder="Address &amp; Borough | Member | Neighborhood" />
+                <input type="text" aria-label="Search for you district and council member" id="list-search-input" class="-no-margin -search -search--no-submit" placeholder="Address &amp; Borough | Member | Neighborhood" />
                 <span class="district-submit" onClick="jQuery('#list-search').submit();" style="color: #666; cursor: pointer; position: absolute; top: 7px; right: 10px;"><i class="fa fa-search" aria-hidden="true"></i></span>
               </form>
               <table class="full-width">
