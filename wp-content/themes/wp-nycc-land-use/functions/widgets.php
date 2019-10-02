f189cb7a1d2e53ed1e6826209c9a7fead4b20e07
JC-NYCC-1227 <jchei@council.nyc.gov>
2019-10-02 11:32:18 -0400

    land use acccessibility

--- wp-content/themes/wp-nycc-land-use/functions/widgets.php
+++ wp-content/themes/wp-nycc-land-use/functions/widgets.php
@@ -9,8 +9,8 @@ function nycc_register_sidebars() {
             'description' => __( 'Sidebar Widget Area', 'nycc' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
-            'before_title' => '<h4 class="widget-title">',
-            'after_title' => '</h4>'
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>'
         )
     );
 }
