17d0c6a36254869ce48b42982fed6b5a94f3b265
JC-NYCC-1227 <jchei@council.nyc.gov>
2021-02-23 17:56:19 -0500

    missed some

--- wp-content/themes/wp-nycc/district_header.php
+++ wp-content/themes/wp-nycc/district_header.php
@@ -13,8 +13,8 @@
       <div class="image-overlay">
         <div class="row">
           <div class="columns clearfix">
-            <h2 class="image-overlay-text district-number"><a href="<?php echo esc_url( network_site_url() ); ?>district-<?php echo get_option('council_district_number'); ?>/">District&nbsp;<?php echo get_option('council_district_number'); ?></a></h2>
-            <h2 class="image-overlay-text district-member"><a href="<?php echo site_url(); ?>/"><?php echo get_option('council_member_name'); ?></a></h2>
+            <h1 class="image-overlay-text district-number"><a href="<?php echo esc_url( network_site_url() ); ?>district-<?php echo get_option('council_district_number'); ?>/">District&nbsp;<?php echo get_option('council_district_number'); ?></a></h1>
+            <h1 class="image-overlay-text district-member"><a href="<?php echo site_url(); ?>/"><?php echo get_option('council_member_name'); ?></a></h1>
             <p class="image-overlay-text district-neighborhoods show-for-large"><?php echo get_option('council_district_neighborhoods'); ?></p>
           </div>
         </div>
