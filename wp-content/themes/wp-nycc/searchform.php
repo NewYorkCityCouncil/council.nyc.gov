ecf230a66c62622f6d295ac431b9075e9e2714da
JC-NYCC-1227 <jchei@council.nyc.gov>
2021-02-23 17:43:56 -0500

    making the search button visible now

--- wp-content/themes/wp-nycc/searchform.php
+++ wp-content/themes/wp-nycc/searchform.php
@@ -3,10 +3,10 @@
   <label>
     <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
     <input type="search" class="search-field"
-           placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-           value="<?php echo get_search_query() ?>" name="s"
-           title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>"
+      placeholder="<?php echo esc_attr_x( 'Search this site', 'placeholder' ) ?>"
+      value="<?php echo get_search_query() ?>" name="s"
+      title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>"
     />
   </label>
-  <input type="submit" class="search-submit screen-reader-text" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+  <input type="submit" class="button" style="width:100%;" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 </form>
