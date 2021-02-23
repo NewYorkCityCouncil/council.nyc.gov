d00d60b7f1e133da31dea3c3f7216e1b1d7ad2b4
JC-NYCC-1227 <jchei@council.nyc.gov>
2021-02-23 17:19:03 -0500

    tabindex -1 on input field for map search

--- wp-content/themes/wp-nycc/map_scripts.php
+++ wp-content/themes/wp-nycc/map_scripts.php
@@ -231,7 +231,7 @@ if ( is_page_template( 'page-district.php' ) ) {
   addresslookup.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'addresslookup');
     L.DomEvent.disableClickPropagation(div);
-    div.innerHTML = '<form aria-hidden="true" aria-describedby="addresslookup-error" id="addresslookup"><input id="mapAddress" aria-label="Enter street address or borough" type="text" placeholder="Street Address, Borough"><button type="submit" class="dashicons-before dashicons-search" aria-label="Search"><span class="show-for-sr">Search</span></button><div aria-live="assertive" role="alert" id="addresslookup-error" class="addresslookup-error"></div></form>';
+    div.innerHTML = '<form aria-hidden="true" aria-describedby="addresslookup-error" id="addresslookup"><input id="mapAddress" aria-label="Enter street address or borough" type="text" placeholder="Street Address, Borough" tabindex="-1"><button type="submit" class="dashicons-before dashicons-search" aria-label="Search" tabindex="-1"><span class="show-for-sr">Search</span></button><div aria-live="assertive" role="alert" id="addresslookup-error" class="addresslookup-error"></div></form>';
     return div;
   };
   addresslookup.addTo(map);
