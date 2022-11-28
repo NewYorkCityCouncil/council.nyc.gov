e5fbf5b2bd0fcf6916bd25d07f213b7a25000f81
JC-NYCC-1227 <jchei@council.nyc.gov>
2022-11-28 16:32:03 -0500

    cleaning up commented out lines

--- wp-content/themes/wp-nycc/header.php
+++ wp-content/themes/wp-nycc/header.php
@@ -52,17 +52,10 @@
   
   <body <?php body_class(); ?>>
     <a style="position: absolute; top:0; left: -10000px;" id="skip-link-a" href="#main">Skip to main content</a>
-    <!-- <script>jQuery("#skip-link-a").focus(function(){jQuery("#skip-link-li").css("position","initial")}).focusout(function(){jQuery("#skip-link-li").css("position","absolute")});</script> -->
     <!-- Google Tag Manager (noscript)
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCN4XTT"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     End Google Tag Manager (noscript) -->
-    <!-- <?php if ( wp_get_theme()->get('Name') == 'NYCC Member' ) {} else { ?>
-    <div id="map-container" class="short" aria-hidden="true">
-      <div id="map"></div>
-      <button class="map-toggler" id="map-toggler">Expand Map</button>
-    </div>
-    <?php } ?> -->
 
     <div id="sticky-wrapper"> <!-- closing tag on line 16 ./footer.php -->
       <div data-sticky-container>
--- wp-content/themes/wp-nycc/page-featured-content.php
+++ wp-content/themes/wp-nycc/page-featured-content.php
@@ -7,7 +7,6 @@
       <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
         <div class="page-header">
           <h1 class="header-xxlarge"><?php the_title(); ?></h1>
-          <!-- <?php if ( has_excerpt( $post->ID ) ) { ?><p class="header-medium subheader sans-serif"><?php echo get_the_excerpt(); ?></p><?php } ?> -->
         </div>
 
         <hr>
@@ -22,53 +21,53 @@
               <?php
                 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
                 $list_features = new WP_Query('post_type=nycc_feature&orderby=menu_order&order=ASC&posts_per_page=10&paged='. $paged);
-                 if ( $list_features->have_posts() ) {
-                  while ( $list_features->have_posts() ) {
-                    $list_features->the_post();     
+                  if ( $list_features->have_posts() ) {
+                    while ( $list_features->have_posts() ) {
+                      $list_features->the_post();     
               ?>
-              <li class="columns" id="feature-<?php the_ID(); ?>">
-                <?php $feature_link_url = get_post_meta($post->ID, 'feature_link_url', true); ?>
-                <a href="<?php echo $feature_link_url; ?>">
-                  <div class="image-header fit-container">
-                    <div class="image-overlay">
-                      <p class="image-overlay-text header-sm sans-serif"><?php echo get_the_date(); ?></p>
-                      <h3 class="image-overlay-text header-xlarge sans-serif"><?php the_title(); ?></h3>
-                    </div>
-                  </div>
-                </a>
-                <style>
-                #feature-<?php the_ID(); ?> .image-header {
-                  margin: 0;
-                }
-                #feature-<?php the_ID(); ?> .image-header::before {
-                  background-image: url("<?php the_post_thumbnail_url( 'small' ); ?>");
-                }
-                /* small retina */
-                @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
-                  #feature-<?php the_ID(); ?> .image-header::before {
-                    background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
+                      <li class="columns" id="feature-<?php the_ID(); ?>">
+                        <?php $feature_link_url = get_post_meta($post->ID, 'feature_link_url', true); ?>
+                        <a href="<?php echo $feature_link_url; ?>">
+                          <div class="image-header fit-container">
+                            <div class="image-overlay">
+                              <p class="image-overlay-text header-sm sans-serif"><?php echo get_the_date(); ?></p>
+                              <h3 class="image-overlay-text header-xlarge sans-serif"><?php the_title(); ?></h3>
+                            </div>
+                          </div>
+                        </a>
+                        <style>
+                          #feature-<?php the_ID(); ?> .image-header {
+                            margin: 0;
+                          }
+                          #feature-<?php the_ID(); ?> .image-header::before {
+                            background-image: url("<?php the_post_thumbnail_url( 'small' ); ?>");
+                          }
+                          /* small retina */
+                          @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
+                            #feature-<?php the_ID(); ?> .image-header::before {
+                              background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
+                            }
+                          }
+                          /* medium */
+                          @media only screen and (min-width: 40.0625em) {
+                            #feature-<?php the_ID(); ?> .image-header::before {
+                              padding-bottom: 56.25%;
+                              background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
+                              margin: 0;
+                            }
+                          }
+                          /* medium retina */
+                          @media only screen and (min-width: 40.0625em) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-width: 40.0625em) and (min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min-resolution: 192dpi), only screen and (min-width: 40.0625em) and (min-resolution: 2dppx) {
+                            #feature-<?php the_ID(); ?> .image-header::before {
+                              background-image: url("<?php the_post_thumbnail_url( 'large' ); ?>");
+                            }
+                          }
+                        </style>
+                      </li>
+                <?php
+                    }
                   }
-                }
-                /* medium */
-                @media only screen and (min-width: 40.0625em) {
-                  #feature-<?php the_ID(); ?> .image-header::before {
-                    padding-bottom: 56.25%;
-                    background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
-                    margin: 0;
-                  }
-                }
-                /* medium retina */
-                @media only screen and (min-width: 40.0625em) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-width: 40.0625em) and (min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min-resolution: 192dpi), only screen and (min-width: 40.0625em) and (min-resolution: 2dppx) {
-                  #feature-<?php the_ID(); ?> .image-header::before {
-                    background-image: url("<?php the_post_thumbnail_url( 'large' ); ?>");
-                  }
-                }
-              </style>
-              </li>
-              <?php
-                }
-               }
-              ?>
+                ?>
             </ul>
           </div>
         </div>
