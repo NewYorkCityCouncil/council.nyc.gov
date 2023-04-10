5ed6f12aa7c675eb4e3481b9c83c90573ce28c79
JC-NYCC-1227 <jchei@council.nyc.gov>
2023-04-10 15:15:40 -0400

    adding title tag to the map widget page

--- wp-content/themes/wp-nycc/page-widget-map.php
+++ wp-content/themes/wp-nycc/page-widget-map.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>District Map Widget - New York City Council</title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cartodb-libs.global.ssl.fastly.net/cartodb.js/v3/3.15/themes/css/cartodb.css" />
