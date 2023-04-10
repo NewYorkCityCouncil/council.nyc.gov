d075a6bb1dba84b92d3c376f15918583ffcf7cb4
Chei <jchei@WDU-MAC-1A6JGH5.local>
2023-04-10 13:05:36 -0400

    use higher res images for hero

--- wp-content/themes/wp-nycc/img_header_style.php
+++ wp-content/themes/wp-nycc/img_header_style.php
@@ -1,29 +1,29 @@
 <style>
   .image-header::before {
-    background-image: url("<?php the_post_thumbnail_url( 'small' ); ?>");
+    background-image: url("<?php the_post_thumbnail_url( 'large' ); ?>");
   }
   /* small retina */
   @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
     .image-header::before {
-      background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
+      background-image: url("<?php the_post_thumbnail_url( 'large' ); ?>");
     }
   }
   /* medium */
   @media only screen and (min-width: 40.0625em) {
     .image-header::before {
-      background-image: url("<?php the_post_thumbnail_url( 'medium' ); ?>");
+      background-image: url("<?php the_post_thumbnail_url( 'xlarge' ); ?>");
     }
   }
   /* medium retina */
   @media only screen and (min-width: 40.0625em) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-width: 40.0625em) and (min-device-pixel-ratio: 2), only screen and (min-width: 40.0625em) and (min-resolution: 192dpi), only screen and (min-width: 40.0625em) and (min-resolution: 2dppx) {
     .image-header::before {
-      background-image: url("<?php the_post_thumbnail_url( 'large' ); ?>");
+      background-image: url("<?php the_post_thumbnail_url( 'xlarge' ); ?>");
     }
   }
   /* large */
   @media only screen and (min-width: 64.0625em) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 64.0625em) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 64.0625em) and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-width: 64.0625em) and (min-device-pixel-ratio: 2), only screen and (min-width: 64.0625em) and (min-resolution: 192dpi), only screen and (min-width: 64.0625em) and (min-resolution: 2dppx) {
     .image-header::before {
-      background-image: url("<?php the_post_thumbnail_url( 'xlarge' ); ?>");
+      background-image: url("<?php the_post_thumbnail_url(); ?>");
     }
   }
 </style>
