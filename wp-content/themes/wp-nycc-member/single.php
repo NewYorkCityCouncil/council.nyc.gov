8ea5259253b6d19b54e136dcd4209d0e56b4bf0b
JC-NYCC-1227 <jchei@council.nyc.gov>
2023-06-22 10:47:57 -0400

    removing by-line from news posts

--- wp-content/themes/wp-nycc-member/single.php
+++ wp-content/themes/wp-nycc-member/single.php
@@ -9,7 +9,7 @@
 
           <div class="post-header">
             <h1 class="post-title single-title"><?php the_title(); ?></h1>
-            <p class="byline"><?php if ( function_exists( 'get_wp_user_avatar' ) ) { echo get_wp_user_avatar( get_the_author_meta('ID'), 'small' ); } ?>&nbsp;&nbsp;<?php the_author(); ?>&nbsp;<span class="meta-separator">|</span> <?php the_time('F j, Y') ?></p>
+            <p class="byline"><?php the_time('F j, Y') ?></p>
           </div>
 
           <section class="post-content">
