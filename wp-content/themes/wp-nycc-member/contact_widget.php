5065e82d7ae865b8dea6f4df43bed984e8ffc0bc
JC-NYCC-1227 <jchei@council.nyc.gov>
2024-01-10 14:32:58 -0500

    commenting out the 'Get Assistance' form

--- wp-content/themes/wp-nycc-member/contact_widget.php
+++ wp-content/themes/wp-nycc-member/contact_widget.php
@@ -16,16 +16,16 @@ if ( $legislative_contact ) { ?><div aria-label="Legislative office contact info
 
 if ( $email ) { ?><a aria-label="Send an email to Council Member <?php echo get_option('council_member_name'); ?>" href="mailto:<?php echo $email; ?>" class="button secondary expanded dashicons-before dashicons-email-alt">&nbsp;Send&nbsp;Email<br class="show-for-large"><small class="show-for-xlarge" style="font-size:0.5em;"><?php echo $email; ?></small></a><?php }
 
-if ( $contact_form ) {
+/*if ( $contact_form ) {
     ?>
     <div class="reveal" id="contact_form" data-reveal>
       <h2 class="header-small">Send a message to <?php echo get_option('council_member_name'); ?></h2>
-      <?php echo do_shortcode($contact_form); ?>
+      <#?php echo do_shortcode($contact_form); ?>
       <button class="close-button" data-close aria-label="Close modal" type="button"><span aria-hidden="true">&times;</span></button>
     </div>
-    <a<?php if ($is_main) {?> href="<?php echo $siteurl; ?>?contact=message"<?php } else { ?> data-open="contact_form"<?php } ?> class="button secondary expanded dashicons-before dashicons-admin-comments">&nbsp;Get&nbsp;Assistance</a>
-    <?php
-}
+    <a<#?php if ($is_main) {?> href="<#?php echo $siteurl; ?>?contact=message"<#?php } else { ?> data-open="contact_form"<#?php } ?> class="button secondary expanded dashicons-before dashicons-admin-comments">&nbsp;Get&nbsp;Assistance</a>
+    <#?php
+}*/
 
 if ( $subscribe_form ) {
     ?>
