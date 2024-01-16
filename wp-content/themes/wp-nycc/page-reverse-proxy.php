b034b4bc1f3a328dba8962dd46ba0fcc9fa40a91
Alaa Moussawi <alaamoussawi@gmail.com>
2024-01-16 15:25:14 -0500

    Update page-reverse-proxy.php

--- wp-content/themes/wp-nycc/page-reverse-proxy.php
+++ wp-content/themes/wp-nycc/page-reverse-proxy.php
@@ -1,3 +1,25 @@
+<?php
+/* Template Name: Reverse Proxy Template */
+
+$uri = $_SERVER['REQUEST_URI'];
+$forward_uri = explode("forward_uri=", $uri);
+
+if (count($forward_uri) == 1) {
+    $external_url = "https://council.nyc.gov/";
+} else {
+    $external_url = $forward_uri[1];
+}
+
+$external_content = file_get_contents($external_url);
+
+// Replace relative paths in the HTML content
+$external_content = str_replace('href="/', 'href="' . $external_url, $external_content);
+$external_content = str_replace('src="/', 'src="' . $external_url, $external_content);
+
+echo $external_content;
+?>
+
+/*
 <?php /* Template Name: Reverse Proxy Template */
     $uri = $_SERVER['REQUEST_URI'];
     $forward_uri = explode("forward_uri=", $uri);
@@ -10,3 +32,4 @@
     $external_content = file_get_contents($external_url);
     echo $external_content;
 ?>
+*/
