e64fe330c60fb13d55dac5613d3edba9b286af9a
Alaa Moussawi <alaamoussawi@gmail.com>
2024-01-18 17:26:36 -0500

    Update page-reverse-proxy.php

--- wp-content/themes/wp-nycc/page-reverse-proxy.php
+++ wp-content/themes/wp-nycc/page-reverse-proxy.php
@@ -12,24 +12,11 @@ if (count($forward_uri) == 1) {
 
 $external_content = file_get_contents($external_url);
 
-// Replace relative paths in the HTML content
+// Replace URLs in the fetched content
 $external_content = str_replace('href="/', 'href="' . $external_url, $external_content);
 $external_content = str_replace('src="/', 'src="' . $external_url, $external_content);
+$external_content = str_replace('../../', 'https://datateam.council.nyc.gov:8377/expense_funding/', $external_content);
 
 echo $external_content;
-?>
 
-/*
-<?php // Template Name: Reverse Proxy Template
-    $uri = $_SERVER['REQUEST_URI'];
-    $forward_uri = explode("forward_uri=", $uri);
-    if (count($forward_uri) == 1){
-        $external_url = "https://council.nyc.gov/";
-    } else {
-        $external_url = $forward_uri[1];
-    }
- 
-    $external_content = file_get_contents($external_url);
-    echo $external_content;
 ?>
-*/
