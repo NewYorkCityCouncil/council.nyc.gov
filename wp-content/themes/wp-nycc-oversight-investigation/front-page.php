2cdb44f71e0bdd94539556b959d14e067c028f6d
JC-NYCC-1227 <jchei@council.nyc.gov>
2024-09-04 17:35:16 -0400

    naming the theme for data and compliance, fixing a critical error, + theme for O&I

---
+++ wp-content/themes/wp-nycc-oversight-investigation/front-page.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+<?php if ( has_post_thumbnail() ) {
+    get_template_part( 'img_header_style' );
+  ?>
+  <div class="image-header">
+    <div class="page-header image-overlay-large">
+      <div class="row">
+        <div class="columns clearfix">
+          <h1 class="image-overlay-text header-xxlarge"><?php the_title(); ?></h1>
+          <?php if ( has_excerpt( $post->ID ) ) { ?><p class="image-overlay-text header-medium sans-serif"><?php echo get_the_excerpt(); ?></p><?php } ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php } ?>
+  
+  <div class="row">
+    <div class="columns medium-8">
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
+
+        <div class="page-header">
+          <h1><?php the_title(); ?></h1>
+          <hr>
+        </div>
+
+        <section class="page-content">
+          <?php the_content(); ?>
+        </section>
+
+      </article>
+
+      <?php endwhile; endif; ?>
+
+    </div>
+
+    <?php get_sidebar(); ?>
+
+  </div>
+
+<?php get_footer(); ?>
